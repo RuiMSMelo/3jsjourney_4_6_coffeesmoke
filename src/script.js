4f135b4475a2701ee504fdedfc6280f3bbf455b3
Rui Melo <ruimsmelo@gmail.com>
2025-05-01 20:20:01 +0100

    smoke done with twist and wind

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import GUI from 'lil-gui'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import * as THREE from "three"
+import { OrbitControls } from "three/addons/controls/OrbitControls.js"
+import GUI from "lil-gui"
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js"
+import coffeSmokeVertexShader from "./shaders/coffeSmoke/vertex.glsl"
+import coffeSmokeFragmentShader from "./shaders/coffeSmoke/fragment.glsl"
 
 /**
  * Base
@@ -10,7 +12,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 const gui = new GUI()
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl")
 
 // Scene
 const scene = new THREE.Scene()
@@ -23,30 +25,34 @@ const gltfLoader = new GLTFLoader()
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+	width: window.innerWidth,
+	height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+	// Update sizes
+	sizes.width = window.innerWidth
+	sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+	// Update camera
+	camera.aspect = sizes.width / sizes.height
+	camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+	// Update renderer
+	renderer.setSize(sizes.width, sizes.height)
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(
+	25,
+	sizes.width / sizes.height,
+	0.1,
+	100
+)
 camera.position.x = 8
 camera.position.y = 10
 camera.position.z = 12
@@ -61,8 +67,8 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
+	canvas: canvas,
+	antialias: true,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -70,32 +76,57 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Model
  */
-gltfLoader.load(
-    './bakedModel.glb',
-    (gltf) =>
-    {
-        gltf.scene.getObjectByName('baked').material.map.anisotropy = 8
-        scene.add(gltf.scene)
-    }
-)
+gltfLoader.load("./bakedModel.glb", (gltf) => {
+	gltf.scene.getObjectByName("baked").material.map.anisotropy = 8
+	scene.add(gltf.scene)
+})
+
+/**
+ * Smoke
+ */
+const smokeGeometry = new THREE.PlaneGeometry(1, 1, 16, 64)
+smokeGeometry.translate(0, 0.5, 0)
+smokeGeometry.scale(1.5, 6, 1.5)
+
+const perlinTexture = textureLoader.load("./perlin.png")
+perlinTexture.wrapS = THREE.RepeatWrapping
+perlinTexture.wrapT = THREE.RepeatWrapping
+
+const smokeMaterial = new THREE.ShaderMaterial({
+	vertexShader: coffeSmokeVertexShader,
+	fragmentShader: coffeSmokeFragmentShader,
+	uniforms: {
+		uTime: new THREE.Uniform(0),
+		uPerlinTexture: new THREE.Uniform(perlinTexture),
+	},
+	side: THREE.DoubleSide,
+	transparent: true,
+	depthWrite: false,
+	// wireframe: true,
+})
+
+const smoke = new THREE.Mesh(smokeGeometry, smokeMaterial)
+smoke.position.y = 1.83
+scene.add(smoke)
 
 /**
  * Animate
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+	const elapsedTime = clock.getElapsedTime()
+	// Update smoke
+	smokeMaterial.uniforms.uTime.value = elapsedTime
 
-    // Update controls
-    controls.update()
+	// Update controls
+	controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+	// Render
+	renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+	// Call tick again on the next frame
+	window.requestAnimationFrame(tick)
 }
 
 tick()
